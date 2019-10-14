c422ca981ede6677931e69d715cab3b5c5e6a2a3
jintomas <tomas.jin@outlook.com>
2019-10-14 19:55:59 +0800

    Refactored

--- js/china/analytics.js
+++ js/china/analytics.js
@@ -3,6 +3,34 @@ analytics = {
 };
 
 $(document).ready(function () {
+
+    let diceContainer = null;
+
+    var observer = new MutationObserver(function (mutations) {
+        if (diceContainer == null) {
+            let found = $('#monopolyBase').find('.container-dice');
+            if (found.length) {
+                diceContainer = found[0];
+            }
+        }
+        if (diceContainer == null) {
+            return;
+        }
+
+        diceContainer.style.visibility = $("#popupBackground").is(":visible") ? "hidden" : "visible"
+        diceContainer.style.visibility = $("#resultBackground").is(":visible") ? "hidden" : "visible"
+    });
+
+    var target = document.querySelector("#popupBackground");
+    observer.observe(target, {
+        attributes: true
+    });
+
+    target = document.querySelector("#resultBackground");
+    observer.observe(target, {
+        attributes: true
+    });
+
     function before(object, method, callback) {
         let originalMethod = object[method];
         object["old_" + method] = originalMethod;
@@ -16,6 +44,7 @@ $(document).ready(function () {
 
     function after(object, method, callback) {
         let originalMethod = object[method];
+        object["old_" + method] = originalMethod;
         object[method] = function () {
             originalMethod.apply(object, arguments);
             callback.apply(object);
@@ -29,23 +58,25 @@ $(document).ready(function () {
         return true;
     });
 
-    after(monopoly, 'openAddPlayer', ()=>{
+    after(monopoly, 'openAddPlayer', () => {
 
-        $.get({url:'/getOrg', success: function (data) {
-            if(localStorage.getItem('orgs') == undefined || JSON.parse(localStorage.getItem('orgs')).length != data.length){
-                localStorage.setItem('orgs', JSON.stringify(data))
+        $.get({
+            url: '/getOrg', success: function (data) {
+                if (localStorage.getItem('orgs') == undefined || JSON.parse(localStorage.getItem('orgs')).length != data.length) {
+                    localStorage.setItem('orgs', JSON.stringify(data))
+                }
+                $('#playerOrg')
+                    .find('option')
+                    .remove()
+                    .end();
+                console.log(localStorage.getItem('orgs'))
+                localOrg = JSON.parse(localStorage.getItem('orgs'))
+                for (let i = 0; i < localOrg.length; i++) {
+                    console.log(localOrg[i])
+                    $('#playerOrg').append('<option value="' + localOrg[i].name + '" id="' + localOrg[i].name + localOrg[i].id + '">' + localOrg[i].name + '</option>')
+                }
             }
-            $('#playerOrg')
-                .find('option')
-                .remove()
-                .end();
-            console.log(localStorage.getItem('orgs'))
-            localOrg = JSON.parse(localStorage.getItem('orgs'))
-            for(let i=0; i<localOrg.length; i++) {
-                console.log(localOrg[i])
-                $('#playerOrg').append('<option value="' + localOrg[i].name + '" id="'+ localOrg[i].name + localOrg[i].id+'">' + localOrg[i].name + '</option>')
-            }
-        }})
+        })
     })
 
     /*after(monopoly, 'storePlayerDetails', ()=>{
@@ -63,28 +94,28 @@ $(document).ready(function () {
         }
     })*/
 
-    after(ubsApp, 'closeGame', ()=>{
-        for(var k=0; k<ubsApp.studentArray.length; k++){
+    after(ubsApp, 'closeGame', () => {
+        for (var k = 0; k < ubsApp.studentArray.length; k++) {
             var std = ubsApp.studentArray[k]
-            var url_param = "?userid="+std.StudentName+"&age="+std.StudentAge+
-                "&gender="+std.StudentGender+"&orgname="+std.StudentOrg+"&userdata='"
-            for(var i = 0; i<userArray.length; i++){
-                if(userArray[i].name == ubsApp.studentArray[k].StudentName){
+            var url_param = "?userId=" + std.StudentName + "&age=" + std.StudentAge +
+                "&gender=" + std.StudentGender + "&orgName=" + std.StudentOrg + "&data='"
+            for (var i = 0; i < userArray.length; i++) {
+                if (userArray[i].name == ubsApp.studentArray[k].StudentName) {
                     stdData = JSON.parse(ubsApp.studentArray[k].StudentData)
                     stdData.lastScore = userArray[i].score - initialPlayerCash +
                         userArray[i].bankBalance - initialPlayerBankBalance - userArray[i].credit
-                    if(stdData.lastScore > stdData.highScore){
+                    if (stdData.lastScore > stdData.highScore) {
                         stdData.highScore = stdData.lastScore
                     }
                     ubsApp.studentArray[k].StudentData = JSON.stringify(stdData)
                     url_param = url_param + ubsApp.studentArray[k].StudentData + "'"
                     console.log(url_param)
                     $.get({
-                        url: '/addUser'+url_param,
-                        success:(data) =>{
+                        url: '/addUser' + url_param,
+                        success: (data) => {
                             console.log(data)
                         },
-                        error: (err)=>{
+                        error: (err) => {
                             console.log(err)
                         }
                     })
@@ -123,11 +154,11 @@ analytics.logIn = function () {
     $.post({
         url: analytics.url + "/login",
         data: JSON.stringify(credentials),
-        contentType: 'application/json; charset=utf-8',     
+        contentType: 'application/json; charset=utf-8',
         success: (response) => {
             eval(response);
         }
     });
 };
 
-analytics.submitData = function (data) {};
+analytics.submitData = function (data) { };
